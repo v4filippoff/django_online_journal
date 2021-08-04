efc483b59ea5c3e4dde489d23ab6f712a98fb85f
v4filippoff <v4filippoff@yandex.ru>
2021-08-04 16:03:49 +0300

    Added new model Profile

--- django_project/accounts/models.py
+++ django_project/accounts/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User')
+    register_date = models.DateField('Register date', auto_now_add=True)
+    avatar = models.ImageField(upload_to='user_uploads/')
+    
